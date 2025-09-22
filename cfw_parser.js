3ecad4cde2be764f83d3141aa8bcdc71d140dcfd
Huffer342-WSH <718007138@qq.com>
2025-09-22 11:25:42 +0800

    DOMAIN,arthurchiao.art,默认代理
    Signed-off-by: Huffer342-WSH <718007138@qq.com>

--- cfw_parser.js
+++ cfw_parser.js
@@ -270,6 +270,7 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
         'DOMAIN-SUFFIX,haita.io,默认代理',
         'DOMAIN-SUFFIX,eehk.net,默认代理',
         'DOMAIN-SUFFIX,subxiandan.top,默认代理',
+        'DOMAIN,arthurchiao.art,默认代理',
 
         //chatgpt
         'DOMAIN-SUFFIX,chatgpt.com,chatgpt',
